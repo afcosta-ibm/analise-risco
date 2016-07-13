4f221fb6ecaefceaa9c46862f8c0d540e5273f11
eduardochiote <eduardochiote@github.com>
2016-07-12 21:19:29 -0300

    .

--- grupo-1/AR_lista1_q4.R
+++ grupo-1/AR_lista1_q4.R
@@ -1,13 +1,13 @@
-#Usando simulação, obtenha uma aproximação empírica para a distribuição de probabilidade
+#Usando simula??o, obtenha uma aproxima??o emp?rica para a distribui??o de probabilidade
 #da soma de 12 VAs uniformes 0..1.
 
 
-#Sejam X1,...,X12 variáveis uniforme no intervalo [0,1] e Z = X1+...+X12.
-y = 1:1000
+#Sejam X1,...,X12 vari?veis uniforme no intervalo [0,1] e Z = X1+...+X12.
+y <- 1:1000
 
 for (i in 1:1000) {
- X = runif(12,0,1)
- Z[i] = sum(X)
+ X <- runif(12,0,1)
+ Z[i] <- sum(X)
 }
 
 hist(Z)
