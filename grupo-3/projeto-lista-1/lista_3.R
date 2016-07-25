0587256ccd45c859926bbdd1eccc6f0d19f58140
Marcos Vinicius Monteiro Araujo <marcosaraujo.mvma@gmail.com>
2016-07-25 15:03:28 -0300

    Update lista_3.R

--- grupo-3/projeto-lista-1/lista_3.R
+++ grupo-3/projeto-lista-1/lista_3.R
@@ -1,7 +1,7 @@
-#LISTA 3 SIMULAÇÃO GASTO DIARIO USANDO O TCL
+#LISTA 3 SIMULAÇÃO FATURAMENTO DIARIO USANDO O TCL
 library(triangle)
 
-valor<-rtriangle(360,40,120,60)
+valor<-rtriangle(360,40,120,60) 
 fatu<-rtriangle(360,.15,.30,.22)
 grupos<-rtriangle(3000,90,250,130)
 
@@ -13,6 +13,6 @@ var_valor<-var(valor)
 var_fatu<-var(fatu)
 var_grupos<-var(grupos)
 
-total <- med_grupos * med_fatu * med_grupos
+total <- med_grupos * med_fatu * med_grupos #FATURAMENTO para um dia
 total360 = total * 360 #faturamento anual
 total30= total * 30 #faturamento mensal
