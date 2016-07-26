1bdeca863cc278e04ee3119bc39c1e9a804f0219
alunos <alunos>
2016-07-26 09:50:30 -0300

    foo bar

--- grupo-1/AR-problema-almoco-executivos.R
+++ grupo-1/AR-problema-almoco-executivos.R
@@ -1,3 +1,5 @@
 # para 3000 amostras, sortear um numero entre 16 e 22 e 
 # calcular a soma do almoco.
 # custo do almoco = numero executivos * custo do pedido
+
+#usar o sample para amostrar o numero de sextas sample()
