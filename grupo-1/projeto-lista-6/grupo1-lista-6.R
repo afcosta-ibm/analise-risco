92653e24f06e2aa7e2cee79b98ea58047de558ba
afilcosta <afcosta@br.ibm.com>
2016-09-06 09:26:06 -0300

    afcosta - acerto graficos

--- grupo-1/projeto-lista-6/grupo1-lista-6.R
+++ grupo-1/projeto-lista-6/grupo1-lista-6.R
@@ -31,6 +31,56 @@ hist(secao1.prazos, main="Seção 1 Prazos - Prédio", xlab = "Duração em mese
 # plota o histograma dos prazos da secao 1
 hist(secao1.custos, main="Seção 1 Custos - Prédio", xlab = "Custo em US$",
      ylab = "Frequência")
+     
+# cria o range de percentuais de 5 em 5%
+percentuais <- (seq(0,1,by=0.05))
+
+# cria a distribuicao dos tempos nas faixas de percentuais
+distribuicao <- quantile(secao1.prazos, probs=percentuais)
+
+# imprime os tempos nas faixas de percentuais
+print(distribuicao)
+
+# pega o valor o orcamento aprovado
+prazoMedio <- round(mean(secao1.prazos), 2)
+
+textoPrazoMedio <- paste("Duração em meses - Prazo Médio:", prazoMedio)
+
+# plota o grafico dos tempos 
+plot(distribuicao, percentuais, type='l', col="blue", 
+     main="Prédio",
+     xlab=textoPrazoMedio, ylab="Percentis", 
+     xlim=c(min(distribuicao), max(distribuicao)))
+
+#define a matriz de distribuicao dos percentuais
+matrizDistribuicaoPercentuais = matrix(nrow=length(percentuais), ncol=2)
+# define a coluna 1 como sendo os tempos 
+matrizDistribuicaoPercentuais[,1] = distribuicao
+# define a coluna 2 como sendo os percentuais 
+matrizDistribuicaoPercentuais[,2] = percentuais
+
+# posicao da matriz do prazo da obra em 85% dos casos
+matrizDistribuicaoPercentuais[18,1]
+
+# risca a linha auxiliar para mostrar o ponto em 85%
+abline(v = matrizDistribuicaoPercentuais[18,1], col = "green")
+abline(h = matrizDistribuicaoPercentuais[18,2], col = "green")
+# marca o ponto
+points (matrizDistribuicaoPercentuais[18,1], matrizDistribuicaoPercentuais[18,2], col = "green", cex = 0.8)
+# escreve uma gracinha
+prazoCom85PorCentoArredondado <- round(matrizDistribuicaoPercentuais[18,1], 2)
+texto <- paste("P=85%, Prazo:", prazoCom85PorCentoArredondado, '(meses)')
+text(matrizDistribuicaoPercentuais[18,1], matrizDistribuicaoPercentuais[21,2], texto, cex = 0.7, col = "black")
+
+# risca a linha auxiliar para mostrar o ponto em 70%
+abline(v = matrizDistribuicaoPercentuais[15,1], col = "red")
+abline(h = matrizDistribuicaoPercentuais[15,2], col = "red")
+# marca o ponto de 70%
+points (matrizDistribuicaoPercentuais[15,1], matrizDistribuicaoPercentuais[15,2], col = "green", cex = 0.8)
+# escreve uma gracinha
+prazoCom85PorCentoArredondado <- round(matrizDistribuicaoPercentuais[15,1], 2)
+texto <- paste("P=70%, Prazo:", prazoCom85PorCentoArredondado, '(meses)')
+text(matrizDistribuicaoPercentuais[15,1], matrizDistribuicaoPercentuais[12,2], texto, cex = 0.7, col = "black")
 
 #'-------------------------------------------------------------------------
 
