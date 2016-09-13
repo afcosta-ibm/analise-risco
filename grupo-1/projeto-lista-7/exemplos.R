dbad9e98f785c6435a882faceae28ce17fc13402
afcosta-ibm <alexandre@gmail.com>
2016-09-13 09:57:12 -0300

    lista 7

--- grupo-1/projeto-lista-7/exemplos.R
+++ grupo-1/projeto-lista-7/exemplos.R
@@ -1,3 +1,7 @@
+source("material-eber/constSA1.R")
+
+ddd <- constSA1()
+
 plot(ddd[,1], ddd[,2])
 
 plot(ddd[,1], ddd[,7])
@@ -11,3 +15,13 @@ boxplot(ddd[,3])
 plot(ddd[,3], ddd[,7])
 
 abline(lm(ddd[,7]~ddd[,3]))
+
+fit <- lm(ddd[,7]~ddd[,3])
+
+plot(fit)
+
+plot(ddd)
+abline(lm(ddd[,7]~ddd[,3]))
+
+
+plot(dddSemContingencia, ddd)
