4bf3e3e10bb0021104e25d48d00b1a6f48ab7900
cris <cris>
2016-09-27 10:54:34 -0300

    qq coisa

--- grupo-1/projeto-lista-7/exemplos-grafo.R
+++ grupo-1/projeto-lista-7/exemplos-grafo.R
@@ -4,6 +4,7 @@ library(igraph)
 G <- make_graph(c(1,2, 1,3, 2,4, 3,4, 3,5, 4,6, 5,7, 6,7), directed = T)
 
 d <- c(1,4,5,7,2,1,1)
+
 p<- all_simple_paths(G, from = 1, to = 7)
 
 # pega o primeiro caminho do all_simple_path
