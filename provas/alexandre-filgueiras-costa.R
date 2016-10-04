f958780ff577de8bcdccd7839d2b52b4c46859d3
alunos <alunos>
2016-10-04 08:34:54 -0300

    prova alexandre

--- provas/alexandre-filgueiras-costa.R
+++ provas/alexandre-filgueiras-costa.R
@@ -1,3 +1,21 @@
-#' Prova de Análise de Risco parte 2 
+#' Prova de Análise de Risco - parte 2 - 7 pontos 
 #' Aluno: Alexandre Filgueiras Costa
-#'
+#' 
+#' Considerações do projeto.
+#' 
+#' Atividades (duração) | (custo):
+#' A - rtriangle(1, 4, 2) | rtriangle(75, 110, 90)
+#' B - rtriangle(5, 7, 6) | rtriangle(120, 190, 150)
+#' C - rtriangle(2, 5, 4) | rtriangle(60, 85, 75)
+#' D - rtriangle(1, 4, 3) | rtriangle(45, 80, 60)
+#' E - rtriangle(4, 7, 5) | rtriangle(160, 240, 200)
+#' F - rtriangle(3, 5, 4) | rtriangle(210, 300, 250)
+#' G - rtriangle(1, 3, 2) | rtriangle(85, 140, 120)
+#' 
+#' - Atividade "F" com 0.25 de ser executada
+#' 
+#' - Coeficiente de correlação entre as atividades:
+#'  - "E" e "F" = 0.70
+#'  - "B" e "D" = 0.40
+#' 
+#' 
